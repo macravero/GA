c088ccb920c4c11b944b0c074e765d18419a2778
Cravero <Martin.Cravero.ctr@sabre.com>
2020-03-10 23:02:05 -0300

    finish bot MVP

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import View from './View'
 import Intro from './components/Intro/Intro'
 import Footer from './components/Footer/Footer'
+import Bot from './components/Bot/Bot'
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  const [pos, setPos] = useState(2)
+  const [counter, setCounter] = useState(5)
+  const [pos, setPos] = useState(0)
   const [fontColor, setFontColor] = useState('white')
 
 
@@ -19,6 +20,7 @@ const App = () => {
       {(counter >= 5) && <Navbar setPos={setPos} pos={pos} fontColor={fontColor} />}
       {(counter >= 5) && <View pos={pos} setPos={setPos} setFontColor={setFontColor} fontColor={fontColor} />}
       {(counter >= 5) && <Footer setPos={setPos} fontColor={fontColor} />}
+      {(counter >= 5) && <Bot/>}
     </div>
   );
 }
